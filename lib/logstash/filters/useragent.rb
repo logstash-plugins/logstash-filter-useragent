d632cac81b45404fa03944707dbb0afbf9c757ea
David M. Lee <leedm777@yahoo.com>
2015-09-15 14:32:56 -0500

    Update useragent.rb
    Fixed a typo

--- lib/logstash/filters/useragent.rb
+++ lib/logstash/filters/useragent.rb
@@ -115,7 +115,7 @@ class LogStash::Filters::UserAgent < LogStash::Filters::Base
 
     target[@prefix + "name"] = ua_data.name.dup.force_encoding(Encoding::UTF_8)
 
-    #OSX, Andriod and maybe iOS parse correctly, ua-agent parsing for Windows does not provide this level of detail
+    #OSX, Android and maybe iOS parse correctly, ua-agent parsing for Windows does not provide this level of detail
 
     # Calls in here use #dup because there's potential for later filters to modify these values
     # and corrupt the cache. See uap source here for details https://github.com/ua-parser/uap-ruby/tree/master/lib/user_agent_parser
