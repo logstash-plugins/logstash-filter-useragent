02f06e718732dcdb558e489d41109c6552d61ebe
Mathieu Lecarme <mlecarme@bearstech.com>
2015-11-19 21:57:45 +0100

    Simple Linux and OS name.

--- lib/logstash/filters/useragent.rb
+++ lib/logstash/filters/useragent.rb
@@ -9,6 +9,13 @@ require "tempfile"
 # UserAgent filter, adds information about user agent like family, operating
 # system, version, and device
 #
+
+LINUX = ["Ubuntu", "Kubuntu", "Arch Linux", "CentOS", "Slackware", "Gentoo",
+         "openSUSE", "SUSE", "Red Hat", "Fedora", "PCLinuxOS", "Gentoo",
+         "Mageia", "BackTrack", "Lubuntu", "Puppy"]
+SIMPLE_OS = ["iOS", "Mac OS X", "Android", "Windows Phone", "Windows",
+             "BlackBerry", "Symbian"]
+
 # Logstash releases ship with the regexes.yaml database made available from
 # ua-parser with an Apache 2.0 license. For more details on ua-parser, see
 # <https://github.com/tobie/ua-parser/>.
@@ -137,6 +144,15 @@ class LogStash::Filters::UserAgent < LogStash::Filters::Base
       if (os_version = os.version)
         target[@prefix + "os_major"] = os_version.major.dup.force_encoding(Encoding::UTF_8) if os_version.major
         target[@prefix + "os_minor"] = os_version.minor.dup.force_encoding(Encoding::UTF_8) if os_version.minor
+      end
+
+      simple_name = SIMPLE_OS.find {|e| target[@prefix + "os_name"].start_with?(e) }
+      if simple_name
+        target[@prefix + "os_simple_name"] = simple_name
+      elsif LINUX.include? target[@prefix + "os_name"]
+        target[@prefix + "os_simple_name"] = "Linux"
+      else
+        target[@prefix + "os_simple_name"] = target[@prefix + "os_name"]
       end
     end
 
